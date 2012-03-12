5db6c09afcc80b24d65ce21c59ad2c6b9551798a
Fabio Rattis <frattis@shcnet.com.br>
2012-03-12 16:35:17 -0300

    versao sem o injetor

---
+++ Biblioteca.WebApplication/View/Configuracoes/InserirDadosFormulario.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="InserirDadosFormulario.aspx.cs" Inherits="Biblioteca.WebApplication.View.Configuracoes.InserirDadosFormulario" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>
+        criar dados</h2>
+    <table>
+        <tr>
+            <td colspan="2">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Nome do Livro
+            </td>
+            <td>
+                <asp:TextBox ID="txbLivro" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Autor
+            </td>
+            <td>
+                <asp:TextBox ID="txbAutor" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+    </table>
+    <br />
+    <asp:Button ID="Button1" runat="server" Text="Salvar" onclick="Button1_Click" />
+</asp:Content>
---
+++ Biblioteca.WebApplication/View/Configuracoes/InserirDadosFormulario.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Biblioteca.Dominio.Repositorio;
+using Biblioteca.Dominio.Entidades;
+using Biblioteca.NHibernate.NHibernateHelpers;
+
+namespace Biblioteca.WebApplication.View.Configuracoes
+{
+    public partial class InserirDadosFormulario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var provider = new SessionFactoryProvider();
+            var sessionProvider = new SessionProvider(provider);
+            var sessaoAtual = sessionProvider.GetCurrentSession();
+
+            var estante = sessaoAtual.Load<Dominio.Entidades.Estante>(1);
+
+            var prateleira = sessaoAtual.Load<Prateleira>(1);
+
+            var autor = new Autor();
+            autor.Nome = txbAutor.Text;
+            sessaoAtual.SaveOrUpdate(autor);
+
+            var livro = new Livro();
+            livro.Autor = autor;
+            livro.Titulo = txbLivro.Text;
+            prateleira.AdicionarLivros(livro);
+            sessaoAtual.Save(livro);
+
+            ClientScript.RegisterStartupScript(typeof(string), "validate", "<script> alert('salvo com sucesso'); </script>");
+
+            txbAutor.Text = string.Empty;
+            txbLivro.Text = string.Empty;
+        }
+
+    }
+}
